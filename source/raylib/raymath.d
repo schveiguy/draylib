82d1f4f41be7aa01624a0e3dacb08e61658e1ba4
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-10-01 23:02:25 -0400

    Vector3Add

--- source/raylib/raymath.d
+++ source/raylib/raymath.d
@@ -414,7 +414,12 @@ Vector3 Vector3One()
 }
 
 // Add two vectors
-Vector3 Vector3Add (Vector3 v1, Vector3 v2);
+Vector3 Vector3Add(Vector3 v1, Vector3 v2)
+{
+    Vector3 result = { v1.x + v2.x, v1.y + v2.y, v1.z + v2.z };
+
+    return result;
+}
 
 // Add vector and float value
 Vector3 Vector3AddValue (Vector3 v, float add);
