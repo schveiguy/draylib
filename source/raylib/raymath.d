84564e3a86616186ce4c6b82dfa9cb5dea8057c9
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-10-01 23:14:53 -0400

    Update raymath.d

--- source/raylib/raymath.d
+++ source/raylib/raymath.d
@@ -463,7 +463,11 @@ Vector3 Vector3Multiply(Vector3 v1, Vector3 v2)
 
 // Calculate two vectors cross product
 Vector3 Vector3CrossProduct(Vector3 v1, Vector3 v2)
+{
+    Vector3 result = { v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z, v1.x * v2.y - v1.y * v2.x };
 
+    return result;
+}
 // Calculate one vector perpendicular vector
 
 // Cross product between vectors
